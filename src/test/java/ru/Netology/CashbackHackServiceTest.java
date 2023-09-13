df2d42b1ce013816892041498b2acc16b3fa8360
Levashov Andrey <levashov.andrei2018@gmail.com>
2023-09-13 22:14:40 +0300

    Falling test junit4-platform

--- src/test/java/ru/Netology/CashbackHackServiceTest.java
+++ src/test/java/ru/Netology/CashbackHackServiceTest.java
@@ -12,15 +12,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount = 901;
         int actual = service.remain(amount);
-        int expected = 99;
-        assertEquals(actual,expected);
-    }
-    @Test
-    public void shouldReturn1Amount999() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 999;
-        int actual = service.remain(amount);
-        int expected = 1;
-        assertEquals(expected,actual);
+        int expected = 9;
+        assertEquals(actual, expected);
     }
 }
